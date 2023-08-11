f625bcddc1130b509a5af553acec1e103002cb4e
= <=>
2023-08-11 18:13:07 +0200

    sqfd

--- src/models/users.js
+++ src/models/users.js
@@ -28,8 +28,42 @@ const User= mongoose.models("User",{
        }
        ] 
     
+}, {
+    timestamp: true
 })
 
+User.pre("save", async function(next) {
+    if(!this.isModified("password")) {
+        return next();
+    }
+    const hassPass = await bcrypt.hashSync(this.password, 10);
+    return (this.password = hassPass)
+   
+});
+
+User.pre("updateOne", async function(next) {
+    if(!this._update.password) {
+        const hassPass = await bcrypt.hashSync(this._update.password, 10);
+         this.password = hassPass
+    }
+    return next();
+});
+
+User.methods.verifyPassword = function(plaintext) {
+    return bcrypt.compareSync(plaintext, this.password)  
+};
+
+// generate a new token and save user 
+User.methods.generateTokenAndSaveUser = async function() {
+    const authToken = jwt.sign({
+        _id: this._id.toString() 
+    }, process.env.JWT_SECRET, {
+        expiresIn: process.env.ONE_DAYS
+    })
+    this.tokens.push({ authToken })
+    await this.save()
+    return authToken
+}
 
 
 
